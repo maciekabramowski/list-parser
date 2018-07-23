a9c0530395d6982d59c9d64cf661a3dbdfb4815c
Maciek <m.abramowski@wp.pl>
2018-07-23 14:33:16 +0200

    Improved list separation

--- parser.py
+++ parser.py
@@ -2,31 +2,41 @@ from html.parser import HTMLParser
 import pprint
 
 x = "initial"
-y = "initial"
+# y = "initial"
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
 
         global x
-        global y
+        # global y
 
         if tag == 'li':
             x = "li"
+        # if tag == "ul":
+        #     y = "ul"
+        # if tag == "ul":
+        #     ha = open("final.txt","a")
+        #     ha.write("\n" + "-----------------" "\n")
+
+    def handle_endtag(self, tag):
+
         if tag == "ul":
-            y = "ul"
+            ha = open("final.txt","a")
+            ha.write("\n" + "-----------------" "\n")
+
 
     def handle_data(self, data):
         global x
-        global y
+        # global y
 
         ha = open("final.txt","a")
 
         if x == "li":
             ha.write("<li>" + data + "</li>")
             x = "initial"
-        if y == "ul":
-            ha.write("\n" + "-----------------" "\n")
-            y = "initial"
+        # if y == "ul":
+        #     ha.write("\n" + "-----------------" "\n")
+        #     y = "initial"
 
 
 def main():
