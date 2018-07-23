052ab13e8cf03f3b6a7502cf6d6c987559e5e9c0
Maciek <m.abramowski@wp.pl>
2018-07-23 14:35:28 +0200

    cleaned comments

--- parser.py
+++ parser.py
@@ -2,21 +2,14 @@ from html.parser import HTMLParser
 import pprint
 
 x = "initial"
-# y = "initial"
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
 
         global x
-        # global y
 
         if tag == 'li':
             x = "li"
-        # if tag == "ul":
-        #     y = "ul"
-        # if tag == "ul":
-        #     ha = open("final.txt","a")
-        #     ha.write("\n" + "-----------------" "\n")
 
     def handle_endtag(self, tag):
 
@@ -27,16 +20,12 @@ class MyHTMLParser(HTMLParser):
 
     def handle_data(self, data):
         global x
-        # global y
 
         ha = open("final.txt","a")
 
         if x == "li":
             ha.write("<li>" + data + "</li>")
             x = "initial"
-        # if y == "ul":
-        #     ha.write("\n" + "-----------------" "\n")
-        #     y = "initial"
 
 
 def main():
